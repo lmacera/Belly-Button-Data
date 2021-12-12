93d26fe65ebb6b8d626391675de1a9e95eab79d6
Lisa_Macera <lisa.a.macera@gmail.com>
2021-12-12 11:33:56 -0700

    update

--- charts.js
+++ charts.js
@@ -126,7 +126,7 @@ function buildCharts(sample) {
       }];
 
      var gaugeLayout = { 
-      width:500,
+      width:300,
       height:400,
       margin: { t: 25, r: 25, l: 25, b: 25 },
       paper_bgcolor: "black",
